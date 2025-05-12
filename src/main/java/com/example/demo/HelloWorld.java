b25447e49179fb63fdf6cacbd8e1403a063bc178
sayan7003 <96246327+sayan7003@users.noreply.github.com>
2025-05-12 06:58:13 -0700

    Update HelloWorld.java

--- src/main/java/com/example/demo/HelloWorld.java
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -3,5 +3,7 @@ package com.example.demo;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Sayan");
+        
     }
 }
